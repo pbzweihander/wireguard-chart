58e235dccc2aa67a02bb864ed5ce34816a5139bd
pbzweihander <pbzweihander@gmail.com>
2023-10-30 23:36:35 +0900

    Revert and ignore creation failure

--- helm/wireguard/scripts/gen-key.sh
+++ helm/wireguard/scripts/gen-key.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-kubectl --namespace $RELEASE_NAMESPACE create secret generic $SECRET_NAME --from-literal=privatekey=$(wg genkey) --dry-run=client -o yaml | kubectl apply -f -
+kubectl --namespace $RELEASE_NAMESPACE create secret generic $SECRET_NAME --from-literal=privatekey=$(wg genkey) || true
